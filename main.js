33459a5067454b4425512a0c6025dd9026770251
half pace <halfpace64@gmail.com>
2025-10-15 23:18:32 +0530

    added gradient change js fxn

--- main.js
+++ main.js
@@ -21,4 +21,23 @@ themeSwitch.addEventListener("click", () => {
 })
 
 
+//background change function
+//this develops a white colour flicker for every grad change due to class changes so new js code just below this code
+
+const gradients = ["gradient1", "gradient2", "gradient3", "gradient4", "gradient5", "gradient6", "gradient7", "gradient8", "gradient9", "gradient10", "gradient11", "gradient12", "gradient13", "gradient14", "gradient15", "gradient16"];
+let index = 0;
+
+setInterval(() => {
+    //remove current gradient
+    document.body.classList.remove(gradients[index]);
+
+    //move to the next one
+    index = (index + 1) % gradients.length;
+
+    //add the new grad
+    document.body.classList.add(gradients[index]);
+}, 5000);
+
+//new code for above, fix the errors 
+
 //animation functions
